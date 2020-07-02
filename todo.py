43442174989465bf90df70d94cf8d2949b94d912
nesrine <43906541+nesrinebnh@users.noreply.github.com>
2020-07-02 15:06:11 +0100

    final todo add and delete

--- todo.py
+++ todo.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
+
+
 #you may have problem of refresh execute CTRL+SHIFT+R
 
 app=Flask(__name__)
@@ -37,6 +39,15 @@ def index():
         return render_template('index.html', tasks = tasks)
 
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'    
 
 if __name__ == "__main__":
     app.run(debug=True)
