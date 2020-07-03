08b1d90882c08b57dfb4ae97a54dc8821228ea47
nesrine <43906541+nesrinebnh@users.noreply.github.com>
2020-07-03 19:56:26 +0100

    greeting app

--- mnist/service.py
+++ mnist/service.py
@@ -1,11 +1,14 @@
-from flask import request
+from flask import request,render_template
 from flask import jsonify
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello',methods=['POST'])
+@app.route('/')
+def index():
+    return render_template('hello.html')
 
+@app.route('/hello',methods=['POST'])
 def hello():
     message = request.get_json(force=True)
     name = message['name']
@@ -13,3 +16,6 @@ def hello():
         'greeting': 'hello, '+name+'!'
     }
     return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(debug=True)    
