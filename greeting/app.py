325aad05a4864657d6cf1b3835daf0eb61794354
nesrine <43906541+nesrinebnh@users.noreply.github.com>
2020-08-11 22:27:09 +0100

    update

--- greeting/app.py
+++ greeting/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello world'
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
